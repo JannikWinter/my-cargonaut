c11d974be0bd04aea70ecaa8f708f5e16b99ac52
Wengrzinek <wengrzinekm@gmail.com>
2021-01-13 02:22:13 +0100

    Tried an implementation of EditPage.java akin to CarsPage.java

--- app/src/main/java/my_cargonaut/profile/edit/EditPage.java
+++ app/src/main/java/my_cargonaut/profile/edit/EditPage.java
@@ -2,9 +2,12 @@ package my_cargonaut.profile.edit;
 
 import io.javalin.http.Context;
 import my_cargonaut.utility.ProfileEditPage;
+import my_cargonaut.utility.data_classes.user.User;
+
+import java.util.Optional;
 
 public class EditPage extends ProfileEditPage {
-    public static final String PATH = "/editProfile";
+    public static final String PATH = BASEPATH + "/editProfile";
     private final String templateFilePath;
 
     public static final String ProfileEForm = "editProfile";
@@ -16,10 +19,25 @@ public class EditPage extends ProfileEditPage {
     public static final String ProfileECity = "editProfileCity";
     public static final String ProfileECellPhone = "editProfileCellphone";
 
+    private User user;
+
     public EditPage(Context ctx){
         super(ctx);
         templateFilePath="/profile/edit/editProfile.jte";
     }
+
+    public EditPage setUser(User user){
+        this.user = user;
+        return this;
+    }
+    public Optional<User> getUser(){
+        return Optional.ofNullable(user);
+    }
+
+    public static String getDynamicPath(String username) {
+        return PATH.replace(":username", username);
+    }
+
 
     @Override
     public String getTemplate(){
