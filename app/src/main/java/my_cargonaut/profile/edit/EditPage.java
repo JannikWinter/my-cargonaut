b5ca14035664b0569bfe0d4ed3ad256ecaa6c6aa
Wengrzinek <wengrzinekm@gmail.com>
2021-01-13 02:15:16 +0100

    Added the names for EditPage.java

--- app/src/main/java/my_cargonaut/profile/edit/EditPage.java
+++ app/src/main/java/my_cargonaut/profile/edit/EditPage.java
@@ -7,6 +7,14 @@ public class EditPage extends ProfileEditPage {
     public static final String PATH = "/editProfile";
     private final String templateFilePath;
 
+    public static final String ProfileEForm = "editProfile";
+    public static final String ProfileEPronoun = "editProfileDropdown";
+    public static final String ProfileEName = "editProfileName";
+    public static final String ProfileESurname = "editProfileSurname";
+    public static final String ProfileEDOB = "editProfileDOB";
+    public static final String ProfileEPLZ = "editProfilePLZ";
+    public static final String ProfileECity = "editProfileCity";
+    public static final String ProfileECellPhone = "editProfileCellphone";
 
     public EditPage(Context ctx){
         super(ctx);
